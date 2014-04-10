e49fc49079d3162ac9c8daad23a7f9970fc69a57
Kevin Luo <kevin_luo@miiicasa.com>
2014-04-10 19:41:13 +0800

    Add vim install automaticly

--- install.sh
+++ install.sh
@@ -12,13 +12,9 @@ die() {
 
 [ -e "~/.config" ] && die "~/.config already exists."
 
-git clone https://github.com/xfalcons/linux-config.git "$CONFIG_HOME"
-
-cd "$CONFIG_HOME"
-git submodule update --init
+git clone git@github.com:xfalcons/linux-config.git "$CONFIG_HOME"
 
 cd ~
-ln -s .config/.vim .vim
 ln -s .config/.bash_profile .bash_profile
 ln -s .config/.bashrc .bashrc
 ln -s .config/.tmux.conf .tmux.conf
@@ -26,9 +22,7 @@ ln -s .config/.gitconfig .gitconfig
 ln -s .config/git_diff_wrapper git_diff_wrapper
 ln -s .config/.git-completion .git-completion
 
-cd "${CONFIG_HOME}/.vim"
-git submodule update --init
-./install-vimrc.sh
+wget -O - https://raw.github.com/xfalcons/vimrc/master/auto-install.sh | sh
 
 echo "Xfalcons configuration file has been installed."
 
